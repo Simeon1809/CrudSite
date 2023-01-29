4665e12c34d67389a508b165213d076e3e2c1d5f
Simeon1809 <81197471+Simeon1809@users.noreply.github.com>
2023-01-29 09:15:56 +0100

    Update HomeController.cs

--- Activity1/Controllers/HomeController.cs
+++ Activity1/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace Activity1.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+            return View(); 
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
